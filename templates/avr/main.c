c0286f177c30ca768484078606557c995271573f
Waaflee <agustinwafle@gmail.com>
2018-04-28 18:17:48 -0300

    updated template/avr/main.c

--- templates/avr/main.c
+++ templates/avr/main.c
@@ -1,11 +1,19 @@
 // #define POLOLU
 #define NUM_STEPPERS 1
 
-#include "./lib/easyavr/A4988.h"
-#include "./lib/easyavr/A4988_interrups.h"
-#include "./lib/easyavr/core.h"
-
+#include "./lib/AVRDuino/A4988.h"
+#include "./lib/AVRDuino/A4988_interrups.h"
+#include "./lib/AVRDuino/core.h"
+#include <util/delay.h>
 #define FALSE 0
 #define TRUE 1
 
-int main(void) {}
+int main(void) {
+  setPin(13, OUTPUT);
+  while (1) {
+    /* code */
+    togglePin(13);
+    _delay_ms(500);
+  }
+}
+
