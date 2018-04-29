fcfd8231858e6a36ca950af881baa6303e8024b9
Waaflee <agustinwafle@gmail.com>
2018-04-29 17:57:07 -0300

    Improved ardup script for easier file adding

--- templates/avr/main.c
+++ templates/avr/main.c
@@ -1,6 +1,11 @@
 // #define POLOLU
 #define NUM_STEPPERS 1
 
+//volatile int count[NUM_STEPPERS];
+//volatile int delay;
+//STEPPER *PAParray[NUM_STEPPERS];
+
+
 #include "./lib/AVRDuino/A4988.h"
 #include "./lib/AVRDuino/A4988_interrups.h"
 #include "./lib/AVRDuino/core.h"
@@ -9,6 +14,13 @@
 #define TRUE 1
 
 int main(void) {
+  //DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 30};
+  //pololu STP1 = newPololuFA(STPArray1);
+  //STEPPER PAP1;
+  //PAP1.motor = &STP1;
+  //PAP1.enabled = 0;
+  //PAParray[0] = &PAP1;
+
   setPin(13, OUTPUT);
   while (1) {
     /* code */
