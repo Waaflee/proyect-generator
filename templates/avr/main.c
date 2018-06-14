fd02344668e4cc8c6fa6ad4c66e70ce15ac71c00
Waaflee <agustinwafle@gmail.com>
2018-06-14 20:10:32 -0300

    fixed typos

--- templates/avr/main.c
+++ templates/avr/main.c
@@ -1,14 +1,14 @@
 // #define POLOLU
 #define NUM_STEPPERS 1
 
-//volatile int count[NUM_STEPPERS];
-//volatile int delay;
-//STEPPER *PAParray[NUM_STEPPERS];
-
+// volatile int count[NUM_STEPPERS];
+// volatile int delay;
+// STEPPER *PAParray[NUM_STEPPERS];
 
 #include "./lib/AVRDuino/A4988.h"
-#include "./lib/AVRDuino/A4988_interrups.h"
+#include "./lib/AVRDuino/A4988_interrupts.h"
 #include "./lib/AVRDuino/core.h"
+#include "./lib/AVRDuino/uart.h"
 #include <util/delay.h>
 #define FALSE 0
 #define TRUE 1
@@ -19,12 +19,12 @@ int main(void) {
   stdout = stdin = &uart_io;
   UARTcount = 0;
   UART_init();
-  //DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 30};
-  //pololu STP1 = newPololuFA(STPArray1);
-  //STEPPER PAP1;
-  //PAP1.motor = &STP1;
-  //PAP1.enabled = 0;
-  //PAParray[0] = &PAP1;
+  // DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 30};
+  // pololu STP1 = newPololuFA(STPArray1);
+  // STEPPER PAP1;
+  // PAP1.motor = &STP1;
+  // PAP1.enabled = 0;
+  // PAParray[0] = &PAP1;
 
   setPin(13, OUTPUT);
   while (1) {
@@ -34,4 +34,3 @@ int main(void) {
   }
   return 0;
 }
-
