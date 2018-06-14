13351ffeef04372e9c2d3246fd83a6fb9e3a2f6e
Waaflee <agustinwafle@gmail.com>
2018-06-14 18:59:56 -0300

    updated avr/variables.h config file

--- templates/avr/variables.h
+++ templates/avr/variables.h
@@ -1,2 +1,9 @@
-#define NUM_STEPPERS 1
-
+//A4988 congi values, number of simultaneous drivers and amount of stepps for homming
+#define POLOLU
+#ifdef POLOLU
+  #define NUM_STEPPERS 1
+  #define INIT_STEPPS 30000
+#endif
+//Define this if you want uart enabled printf() variable report on certain functions
+#define UART_DEBBUG
+unsigned short int UARTSetted;
